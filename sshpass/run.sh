9292c27e29a6423a4d076d4a1bc65095731afa41
tungnguyensnk <tung.nd194714@sis.hust.edu.vn>
2023-07-09 11:34:55 +0700

    fix sshpass

--- sshpass/run.sh
+++ sshpass/run.sh
@@ -1,5 +1,5 @@
 # define int i
 for i in $(seq 1 100); do
-sshpass -p "$SSH_PASSWORD" ssh -o HostKeyAlgorithms=+ssh-rsa -o PubkeyAcceptedKeyTypes=+ssh-rsa -o KexAlgorithms=+diffie-hellman-group1-sha1 -o StrictHostKeychecking=no -D 40000 -Ng "$SSH_USER"@"$SSH_HOST"
+sshpass -p "$SSH_PASSWORD" ssh -o ServerAliveInterval=120 -o HostKeyAlgorithms=+ssh-rsa -o PubkeyAcceptedKeyTypes=+ssh-rsa -o KexAlgorithms=+diffie-hellman-group1-sha1 -o StrictHostKeychecking=no -D 40000 -Ng4 "$SSH_USER"@"$SSH_HOST"
 echo "SSH Tunnel $i is running"
 done
